cfe408f7ebbf78936eb6dc4eaef9dbedde6e26d8
Garrett Goben <86270631+ArtistXoder@users.noreply.github.com>
2021-06-30 11:31:01 -0500

    Update software.sh

--- software.sh
+++ software.sh
@@ -1,14 +1,18 @@
 # this is my installer scripts for the programs I use on my desktop!
+echo this software included Ruby Classic & gem & linux_logo & OpenRGB & Chromium & Arduino
 sudo apt-get update 
 sudo apt-get upgrade
 sudo snap install core
 sudo snap install ruby --classic
-sudo gem install linux_logo
+sudo gem install linux_logo                                
 cd lolcat-master
-nano linuxlogo.sh
 #!/bin/bash
-echo linuxlogo -a | lolcat
-sh ./linuxlogo.sh
+linuxlogo -a | lolcat
+echo create a file using nano
+echo type nano linuxlogo.sh
+echo type #!/bin/bash
+echo than linuxlogo -a | lolcat
+echo next type chmod +x linuxlogo.sh
 cd 
 mkdir OpenRGB
 wget https://gitlab.com/CalcProgrammer1/OpenRGB/uploads/2fb9ea568772ed99035147a2632f0284/openrgb_0.5_arm64_88464d1.deb
