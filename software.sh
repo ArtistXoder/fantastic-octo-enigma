43cb0b7da4a60f7061ae9b0e5514843fda93f6f2
Garrett Goben <86270631+ArtistXoder@users.noreply.github.com>
2021-06-29 20:15:51 -0500

    Update software.sh

--- software.sh
+++ software.sh
@@ -2,12 +2,12 @@
 sudo snap install core
 sudo snap install ruby --classic
 sudo gem install linux_logo
+cd lolcat-master
+cd
 mkdir OpenRGB
 wget https://gitlab.com/CalcProgrammer1/OpenRGB/uploads/2fb9ea568772ed99035147a2632f0284/openrgb_0.5_arm64_88464d1.deb
 sudo apt install ./openrgb_0.5_arm64_88464d1.deb
-cd
 sudo snap install chromium
-sudo snap install iheartradio
 mkdir Arduino
 wget https://downloads.arduino.cc/arduino-1.8.15-linuxaarch64.tar.xz
 tar -xvf arduino-1.8.15-linuxaarch64.tar.xz
